ac5c917649931300b82448912f26dbd83a1c210f
YoussefMassoud <youssefbusiness133@gmail.com>
2024-07-06 07:05:20 +0300

    fixed hero section work but not 100%

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
+import { Button } from "@/components/ui/button";
 import { client, urlFor } from "@/lib/sanity";
+import Link from "next/link";
 
 async function getData() {
   const query = "*[_type == 'heroImage'][0]";
@@ -11,30 +13,31 @@ async function getData() {
 
 export default async function Hero() {
   const data = await getData();
-
   return (
-    <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-      <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
-        <h1 className="mb-4 text-4xl font-bold text-1 sm:text-5xl md:mb-8 md:text-6xl">
-          Top Fashion for a top price!
-        </h1>
-        <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-          We sell only the most exclusive and high quality products for you. We
-          are the best so come and shop with us.
-        </p>
+    <section className="relative h-screen w-full">
+      <div className="absolute inset-0 z-0">
+        <img
+          src={urlFor(data.image1).url()}
+          alt="Background"
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-
-      <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-        <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-2 shadow-lg md:left-16 md:top-16 lg:ml-0">
-          <img
-            src={urlFor(data.image1).url()}
-            alt="Great Photo"
-            className="h-full w-full object-cover object-center"
-            width={500}
-            height={500}
-          />
+      <div className="absolute inset-0 z-10  flex items-center justify-center bg-black bg-opacity-50">
+        <div className="text-center text-white space-y-7">
+          <h1 className="mb-4 text-4xl font-bold sm:text-5xl md:mb-8 md:text-6xl">
+            Top Fashion for a top price!
+          </h1>
+          <p className="max-w-md leading-relaxed text-gray-200 xl:text-lg mx-auto">
+            We sell only the most exclusive and high quality products for you.
+            We are the best so come and shop with us.
+          </p>
+          <Link href={"/shop"}>
+            <Button className="bg-black text-white hover:bg-black px-8 ">
+              Shop Now
+            </Button>
+          </Link>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
--- lib/sanity.ts
+++ lib/sanity.ts
@@ -4,8 +4,8 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: "omy60mmf",
   dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: true,
+  apiVersion: "2022-03-25", 
+  useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
